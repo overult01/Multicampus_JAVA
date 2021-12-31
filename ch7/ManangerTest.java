3c8820e85f07ecb8b80873703857f712594b6a54
overult01 <67691897+overult01@users.noreply.github.com>
2021-12-31 13:49:34 +0900

    5일차_코드의 재사용을 높이는 상속(extends)
    상속(extends)
    형태: [m] class 이름 [extends 상위 클래스]
    특징
    -단일 상속만 가능(다중 상속 불가)
    -부모랑 이름이 같은 변수가 또 있는 경우 자식(본인) 클래스의 변수값이 우선한다. 물론 부모클래스의 변수도 사용할 수 있다(super키워드로)
    
    다른 클래스의 변수, 메서드가 필요한 경우
    1) is a 관계: 운전자는 사람이다. - 상속관계 사용 
    2) has a 관계: 운전자는 자동차를 가지다. - 객체생성 사용

---
+++ ch7/ManangerTest.java
@@ -0,0 +1,36 @@
+package ch7;
+
+// 상속 예제(extends)
+class Employee{
+	int id;
+	String name;
+	double pay;
+	static String role = "회사원";
+	void work() {
+		System.out.println(id + "사번의 사원명은 " + name + "이고 급여는 " + pay +"입니다.");
+	}
+}
+
+class Manager extends Employee{
+	static String role = "매니저"; // 부모랑 이름이 같은 변수가 또 있다. 이런 경우 자식(본인) 클래스의 변수값이 우선한다.
+	String jobOfManage;
+	void getManage() {
+		System.out.println(name + "매니저는 " + jobOfManage + "입니다.");
+	}
+}
+
+public class ManangerTest {
+
+	public static void main(String[] args) {
+		Manager manager1 = new Manager();
+		manager1.name = "김지원";
+		manager1.jobOfManage = "it매니저";
+		manager1.id = 100;
+		manager1.pay = 123456.78;
+		manager1.work();
+		manager1.getManage();
+		System.out.println(manager1.role);  // 매니저 
+		System.out.println(Manager.role); // 매니저 (static 때문에)
+		System.out.println(Employee.role); // 회사원 (static 때문에)
+	}
+}
