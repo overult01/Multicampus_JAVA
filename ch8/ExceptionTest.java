42d03ae3ea5a146549919d42bdaa2027d314044b
overult01 <67691897+overult01@users.noreply.github.com>
2022-01-05 15:03:37 +0900

    2주_2일차_예외처리(try-catch문)와 예외 강제 발생(throw)
    throw(예외 발생시키기)
    
    예외 발생시키는 순서
    1. 연산자 new를 이용해서 발생시키려는 예외 클래스의 객체를 만든다
    Exception error = new Exception("고의로 에러 발생");
    
    2. 키워드 throw를 이용하여 예외를 발생시킨다.
    throw error;

--- ch8/ExceptionTest.java
+++ ch8/ExceptionTest.java
@@ -1,19 +1,24 @@
 package ch8;
 
-// 예외처리 
+// 예외처리 try - catch 문과 예외 강제 발생시키기 throw
 public class ExceptionTest {
 
 	public static void main(String[] args) {
 		// args배열은 실행할 때 입력받은 값. 
 		// 우리가 입력한 값에 따라 실행할 때도, 실행하지 못할 때도 있다. 
-		int first = 0;
 		// 예외처리1: try - catch 문
 		// try: 실행할 문장, catch: 에러시 실행할 문장 (try문 1개에 다수의 catch블럭이 여러개 올 수 있다. ) 
 		try{
-			first = Integer.parseInt(args[0]);
+			int first = Integer.parseInt(args[0]);
 			int second = Integer.parseInt(args[1]);
 			System.out.println(second / first);
-			}
+//			IllegalArgumentException error = new IllegalArgumentException(); // 예외 강제 생성(일부러 예외 생성) 
+//			throw error; // error 가 참조하고 있는 예외 객체를 강제적 발생시킨다.
+			throw new IllegalArgumentException("매개변수 잘못 지정");  // 위의 두 문장을 한 번에 합치면 이렇게 씀 //throw: 인위적으로 에러를 만들 때, throw 뒤에 에러 객체 써준다. 
+			// second / first 문장 중단 - 예외발생
+			// new ArithmeticException() 자동 생성.
+			// catch 검사 
+		}
 		// 일부 예외만 먼처 처리. 이후 나머지 모든 예외는 catch(Exception error) 로 한 번에 처리도 가능하다.
 		catch(NumberFormatException error) {
 		System.out.println("정수 변환이 가능한 값을 입력하세요");
@@ -21,7 +26,7 @@ public class ExceptionTest {
 
 		// 모든 예외를 한 번에 처리하는 catch: 자바의 모든 에러는 Exception 클래스를 상속받은 것. 따라서 자동형변환 원리로 부모 객체인 Exception 타입사용. 
 		catch(Exception error) {
-			System.out.println("발생하는 모든 에러를 동일 처리");
+			System.out.println(error.getMessage());  
 		}
 		
 		// 예외별 catch 지정. catch(에러명 변수)
@@ -34,22 +39,7 @@ public class ExceptionTest {
 //			error.printStackTrace();
 //			// 예외발생원인 출력 메서드 
 //			System.out.println(error.getMessage());  
-//		}
-//		catch(NumberFormatException error) {
-//			System.out.println("정수 변환이 가능한 값을 입력하세요");
-//		}
-//		catch(ArrayIndexOutOfBoundsException error) {
-//			System.out.println("입력값은 2개 이상이어야 합니다.");
-//		}
-		
+//		}		
 		System.out.println("메인 실행완료");
 	}
 }
-
-/* 
-100 100 입력 : 정상실행
-100 입력 : args[1]이 없어서 ArrayIndexOutOfBoundsException 오류. 
-아무것도 입력 X : ArrayIndexOutOfBoundsException 오류.
-aa bb 입력 : NumberFormatException 오류.
-0 10 입력 : ArithmeticException 오류. 
-*/
