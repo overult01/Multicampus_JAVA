4a7e1bdd4f8844a741bf38bc8d12db3087c39efc
overult01 <67691897+overult01@users.noreply.github.com>
2022-01-10 16:23:25 +0900

    3주_1일차_멀티쓰레드_wait(), notify()
    특정 쓰레드가 중복 실행 불가. 번갈아서 실행될 수 있도록.
    wait(): 기다리는 것
    notify(): wait된 쓰레드에게 재개요청

--- ch13/ProducerConsumer.java
+++ ch13/ProducerConsumer.java
@@ -8,10 +8,10 @@ class Producer extends Thread{
 	public void run(){
 		for(int i = 0; i < 10; i++){
 			// 생산자 스레드는 put() 메서드를 호출한다.
-			blank.put(i);
+			blank.put(i);//생산
 			System.out.println("생산자: 생산 "+ i);
 			try{
-				sleep((int)Math.random() * 100);
+				sleep((int)Math.random() * 100); // sleep(0-100)
 			}catch(InterruptedException e) {}
 		}
 	}
@@ -38,33 +38,33 @@ class Buffer{
 	private boolean available = false;
 
 	//버퍼에서부터 데이터를 가져가는 메서드, 동기화한다.
-	public synchronized int get(){
+	public synchronized int get(){//소비
 		while(available == false){
 			try{
 				wait();
 			} catch(InterruptedException e){}
 		}
-		available = false;
+		available = false;//생산 상태 
 		notifyAll();
 		return contents;
 	}
 	//버퍼로 데이터를 저장하는 메서드, 동기화한다.
-	public synchronized void put(int value){
+	public synchronized void put(int value){//생산
 		while(available == true){
 			try{
 				wait();
 			} catch(InterruptedException e){}
 		}
-		contents = value;
-		available = true;
-		notifyAll();
+		contents = value;// 0
+		available = true;// 생산 데이터 소비 상태
+		notifyAll();//wait 중단 스레드 실행 알려줌
 	}
 }
 //생산자와 소비자 스레드를 기동시키는 클래스
 public class ProducerConsumer{
 	public static void main(String args[]){
 		Buffer ch = new Buffer();
-		Producer p = new Producer(ch);
+		Producer p = new Producer(ch);//ch 객체 공유
 		Consumer c = new Consumer(ch);
 		p.start();
 		c.start();
