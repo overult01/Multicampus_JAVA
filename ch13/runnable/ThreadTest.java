67a2d3fc3f319c02d9ddcc36539e9ed0a312d9ad
overult01 <67691897+overult01@users.noreply.github.com>
2022-01-10 11:35:56 +0900

    3주_1일차_멀티쓰레드(runnable사용: 다중상속 가능)
    Thread.currentThread() : runnable에서 쓰레드 이름을 보고 싶을 때, Thread의 getName()대신 사용 가능. currentThread는 static 메서드라 객체 생성 없이 클래스.static 메서드 로 사용할 수 있다.

--- ch13/runnable/ThreadTest.java
+++ ch13/runnable/ThreadTest.java
@@ -8,6 +8,7 @@ class MyThread1 implements Runnable /*1. 쓰레드 상속받는다*/{
 	// 2.run 메서드 오버라이딩 
 	@Override
 	public void run() {
+		System.out.println(Thread.currentThread().getName()); // 현재 Run을 수행하고 있는 스레드의 이름 확인. 
 		for (int i = 0; i < 100; i++) {
 			System.out.print("!");
 		}
@@ -20,6 +21,8 @@ class MyThread2 implements Runnable {
 	// 2.run 메서드 오버라이딩 
 	@Override
 	public void run() {
+		System.out.println(Thread.currentThread().getName());
+
 		for (int i = 0; i < 100; i++) {
 			System.out.print("#");
 		}
@@ -33,6 +36,7 @@ class MyThread2 implements Runnable {
 public class ThreadTest {
 
 	public static void main(String[] args) {
+		System.out.println(Thread.currentThread().getName());  // main도 하나의 쓰레드이다. 
 		
 		// 3. Runnable 타입 객체 생성 
 		MyThread1 t1 = new MyThread1();
@@ -52,3 +56,4 @@ public class ThreadTest {
 
 }
 
+// Thread.currentThread() : runnable에서 쓰레드 이름을 보고 싶을 때, Thread의 getName()대신 사용 가능. currentThread는 static 메서드라 객체 생성 없이 클래스.static 메서드 로 사용할 수 있다.
